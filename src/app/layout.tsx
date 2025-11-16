abf637a46b1bcd0f12ba128990ac4b861214ec36
Suraj Kumar <surajsk8558@gmail.com>
2025-11-16 16:32:18 +0000

    here is my G-Tag ID  GTM-PS2SG5X8 implement this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/layout/header"
 import Footer from "@/components/layout/footer"
 import { cn } from '@/lib/utils';
 import { Inter, Lexend } from 'next/font/google';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'devSuraj - All-in-One Converter & Productivity Tools',
@@ -30,7 +31,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PS2SG5X8');
+        `}
+      </Script>
       <body className={cn('font-body antialiased', inter.variable, lexend.variable)} suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PS2SG5X8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
